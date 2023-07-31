3bcebd7e12aa8afffa97540d9aed9455434abfed
Rafael Silva <rafsilva004@gmail.com>
2023-07-31 13:19:02 -0500

    created get request

--- server/routes/calculator.router.js
+++ server/routes/calculator.router.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const router = express.Router();
-
+let calculatorEqual = 0
 
 //get request
-
+calculatorRouter.get('/', (req, res) => {
+    console.log('GET request made');
+    res.send(calculatorEqual);
+});
 
 
 
