f8199655ecbd5d931da5165e14c4aaf771f9c9df
Rafael Silva <rafsilva004@gmail.com>
2023-07-31 14:53:51 -0500

    made final edits

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,8 +1,110 @@
-console.log("hi");
+let clientCalculator = '';
 
-let calculatedNumber = '';
-
-function operation(event) {
+function addition() {
+    clientCalculator = '';
+    clientCalculator = '+';
+    console.log('in addition function');
+    console.log(clientCalculator);
+    // Post route sends clientOperator to server
+    axios.post('/operator' , [clientCalculator]).then((response) => {
+        console.log(response);
+     }).catch((error) => {
+         console.log(error);
+         alert('Something went wrong');
+     });
+}
+function subtraction() {
+    clientCalculator = '';
+    clientCalculator = '-';
+    console.log('in subtraction function');
+    console.log(clientCalculator);
+    // Post route sends clientOperator to server
+    axios.post('/operator' ,[clientCalculator]).then((response) => {
+        console.log(response);
+     }).catch((error) => {
+         console.log(error);
+         alert('Something went wrong');
+     });
+}
+function multiplcation() {
+    clientCalculator = '';
+    clientCalculator = '*';
+    console.log('in multiplication function');
+    console.log(clientCalculator);
     
+    axios.post('/operator' , [clientCalculator]).then((response) => {
+        console.log(response);
+     }).catch((error) => {
+         console.log(error);
+         alert('Something went wrong');
+     });
 }
 
+function division() {
+    clientCalculator = '';
+    clientCalculator = '/';
+    console.log('in division function');
+    console.log(clientCalculator);
+    
+    axios.post('/operator' , [clientCalculator]).then((response) => {
+        console.log(response);
+     }).catch((error) => {
+         console.log(error);
+         alert('Something went wrong');
+     });
+}
+
+
+function answer() {
+    let firstNumber = Number(document.querySelector('#firstNumber').value);
+    let secondNumber = Number(document.querySelector('#secondNumber').value);
+    let calulatedInput = [firstNumber, secondNumber];
+    if (clientCalculator === '') {
+        alert('Not Working.')
+        return (null);
+    }
+    axios.post('/inputs' , calulatedInput).then((response) => {
+        console.log(response);
+      
+        getResult();
+        getHistory();
+        clientCalculator = '';
+
+     }).catch((error) => {
+         console.log(error);
+         alert('Something went wrong');
+     });
+}
+
+function getResult() {
+    axios.get('/result').then((response) => {
+        let result = response.data;
+        let resultDiv = document.querySelector('#result');
+        resultDiv.innerText = result;
+    }).catch((error) => {
+        console.log(error);
+        alert('Something went wrong.');
+     });
+}
+
+function getHistory() {
+    axios.get('/history').then((response) => {
+        let history = response.data;
+        let historyDiv = document.querySelector('#history');
+        historyDiv.innerHTML = '';
+        for (let entry of history) {
+            historyDiv.innerHTML += 
+            `<ul><li>${entry.firstNumber} ${entry.operator} ${entry.secondNumber} = ${entry.result}</li></ul>`
+            }
+        }).catch((error) => {
+        console.log(error);
+        alert('Something went wrong.');
+     });
+}
+
+function clearCalculator() {
+    document.querySelector('#firstNumber').value = '';
+    document.querySelector('#secondNumber').value = '';
+    let resultDiv = document.querySelector('#result');
+    resultDiv.innerText = 0;
+}
--- server/server.js
+++ server/server.js
@@ -1,15 +1,68 @@
 const express = require('express');
+
 const app = express();
-const PORT = process.env.PORT || 5001;
-const calculatorRouter = require('./routes/calculator.router');
+const port = process.env.PORT || 5001;
 
-app.use(express.json());
 app.use(express.static('server/public'));
+app.use(express.json());
 
-// ROUTES
-app.use('/calculator', calculatorRouter);
+app.listen(port, () => {
+  console.log('listening on port', port);
+});
 
-// Start listening for requests on a specific port
-app.listen(PORT, () => {
-  console.log('listening on port', PORT);
+
+let history = [];
+let operator = '';
+let result = 0;
+
+app.get('/result', (req, res) => {
+    res.send(result.toString());
+    console.log('Here is the response data', result);
+});
+
+app.get('/history', (req, res) => {
+    res.send(history);
+    console.log('Here is the response data', history);
+});
+
+
+app.post('/operator', (req,res) => {
+    console.log('get a post request.', req.body);
+    let operatorArray = req.body;
+ 
+    operator = operatorArray[0];
+    res.sendStatus(201);
+    console.log(operator);
+});
+
+app.post('/inputs', (req,res) => {
+    console.log('get a post request.', req.body);
+    let firstNumber = req.body[0];
+    let secondNumber = req.body[1];
+
+    console.log(firstNumber);
+    console.log(secondNumber);
+   
+    if (operator === '+') {
+        result = firstNumber + secondNumber;
+    } else if (operator === '-') {
+        result = firstNumber - secondNumber;
+    } else if (operator === '*') {
+        result = firstNumber * secondNumber;
+    } else if (operator === '/') {
+        result = firstNumber / secondNumber;
+    };
+
+   
+    let calculator = {
+        firstNumber: firstNumber,
+        operator: operator,
+        secondNumber: secondNumber,
+        result: result
+    };
+   
+    history.push(calculator);
+
+    console.log(result);
+    res.sendStatus(201);
 });
